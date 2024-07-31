8623d4484bfab515afba396d62017a5d9b0482d4
T-Prakash2002 <prakasht1405@gmail.com>
2024-07-31 11:11:56 +0530

    minor c added

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,4 @@
 
-export const BASE_URL = 'http://localhost:4000/api';
+// export const BASE_URL = 'http://localhost:4000/api';
+
+export const BASE_URL = 'https://tour-management-be.onrender.com/api';
